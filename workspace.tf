1fa7a2539507c086a13f1067341ddeb95081d54c
Damien Aicheh <damienaicheh@microsoft.com>
2024-07-10 12:51:04 +0200

    feat: Update workspace.

--- workspace.tf
+++ workspace.tf
@@ -1,9 +1,24 @@
 module "workspace" {
-  source  = "git::github.com/damienaicheh/modules_wksp_definition?ref=1.2.0" 
-  apim_id = data.azurerm_api_management.apim.id
+  source = "git::github.com/damienaicheh/modules_wksp_definition?ref=1.3.0"
+  apim = {
+    id                  = "TO_DEFINE"
+    name                = "TO_DEFINE"
+    resource_group_name = "TO_DEFINE"
+  }
   workspace = {
     name         = "TO_DEFINE"
     description  = "TO_DEFINE"
     display_name = "TO_DEFINE"
   }
+  products = [
+    {
+      name                  = "TO_DEFINE"
+      approval_required     = false
+      description           = "TO_DEFINE"
+      display_name          = "TO_DEFINE"
+      subscription_required = false
+      subscriptions_limit   = 1
+    }
+    # add more if necessary
+  ]
 }
